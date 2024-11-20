9d7e463bacc7e5658b89eacb92a3802d18ebfc73
Léo Vieira Martins <leovieira6@gmail.com>
2024-11-20 15:31:17 -0300

    elaboração invoice

--- Gerenciamento_Estoque_Desafio_3/src/main/java/estoque/desafio/gerenciamento/controllers/InvoiceController.java
+++ Gerenciamento_Estoque_Desafio_3/src/main/java/estoque/desafio/gerenciamento/controllers/InvoiceController.java
@@ -1,5 +1,79 @@
-package estoque.desafio.gerenciamento.controllers;
+package estoque.desafio.gerenciamento.services;
 
-public class InvoiceController {
+import estoque.desafio.gerenciamento.entities.Invoice;
+import estoque.desafio.gerenciamento.entities.Codigo;
+import estoque.desafio.gerenciamento.entities.DataEmissao;
+import estoque.desafio.gerenciamento.entities.ValorTotalInvoice;
+import estoque.desafio.gerenciamento.repositories.InvoiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+public class InvoiceService {
+
+    @Autowired
+    private InvoiceRepository invoiceRepository;
+
+    
+    public Invoice criarInvoice(Invoice invoice) {
+        return invoiceRepository.save(invoice);
+    }
+
+    public Invoice editarInvoice(Invoice invoice) {
+        return invoiceRepository.save(invoice);
+    }
+
+    public Invoice getInvoice(Long id) {
+        return invoiceRepository.findById(id).orElse(null);
+    }
+
+    public void excluirInvoice(Long id) {
+        invoiceRepository.deleteById(id);
+    }
+
+    public Codigo criarCodigo(Codigo codigo) {
+        return invoiceRepository.saveCodigo(codigo);
+    }
+
+    public Codigo editarCodigo(Codigo codigo) {
+        return invoiceRepository.saveCodigo(codigo);
+    }
+
+    public Codigo getCodigo(Long id) {
+        return invoiceRepository.findCodigoById(id);
+    }
+
+    public void excluirCodigo(Long id) {
+        invoiceRepository.deleteCodigoById(id);
+    }
+
+    
+    public DataEmissao criarDataEmissao(DataEmissao dataEmissao) {
+        return invoiceRepository.saveDataEmissao(dataEmissao);
+    }
+
+    public DataEmissao getDataEmissao(Long id) {
+        return invoiceRepository.findDataEmissaoById(id);
+    }
+
+    public void excluirDataEmissao(Long id) {
+        invoiceRepository.deleteDataEmissaoById(id);
+    }
+
+    
+    public ValorTotalInvoice criarValorTotalInvoice(ValorTotalInvoice valorTotalInvoice) {
+        return invoiceRepository.saveValorTotalInvoice(valorTotalInvoice);
+    }
+
+    public ValorTotalInvoice editarValorTotalInvoice(ValorTotalInvoice valorTotalInvoice) {
+        return invoiceRepository.saveValorTotalInvoice(valorTotalInvoice);
+    }
+
+    public ValorTotalInvoice getValorTotalInvoice(Long id) {
+        return invoiceRepository.findValorTotalInvoiceById(id);
+    }
+
+    public void excluirValorTotalInvoice(Long id) {
+        invoiceRepository.deleteValorTotalInvoiceById(id);
+    }
 }
--- Gerenciamento_Estoque_Desafio_3/src/main/java/estoque/desafio/gerenciamento/services/InvoiceService.java
+++ Gerenciamento_Estoque_Desafio_3/src/main/java/estoque/desafio/gerenciamento/services/InvoiceService.java
@@ -1,5 +1,68 @@
 package estoque.desafio.gerenciamento.services;
 
-public class InvoiceService {
 
+
+@Service
+public class InvoiceService {
+    //public Invoice criarInvoice(Invoice invoice){
+    //      return invoiceRepository.save(invoice);
+    //}
+
+    //public Invoice editarInvoice(Invoice invoice){
+    //    return invoiceRepository.save(invoice);
+    //}
+
+    //public Invoice getInvoice (Long id){
+    //    return invoiceRepository.findByInvoice(id).orElse(null); 
+    //}
+
+    //public void excluirInvoice(Long id) {
+    //    invoiceRepository.deleteById(id);
+    //}
+//Duvida*****
+
+    public Codigo criarCodigo(Codigo codigo){
+        return invoiceRepository.save(codigo);
+    }
+
+    public Codigo editarCodigo(Codigo codigo){
+        return invoiceRepository.save(codigo);
+    }
+
+    public Codigo getCodigo(Codigo codigo){
+        return invoiceRepository.findByCodigo(codigo);
+    }
+
+    public void excluirCodigo(Codigo codigo){
+        return invoiceRepository.deleteByCodigo(codigo);
+    }
+
+    public DataEmissao criarDataEmissao(DataEmissao dataEmissao){
+        return invoiceRepository.save(dataEmissao);
+    }
+
+    public void excluirDataEmissao(DataEmissao dataEmissao){
+        return invoiceRepository.deleteByDataEmissao(dataEmissao);
+    }
+
+    public DataEmissao getDataEmissao(DataEmissao dataEmissao){
+        return invoiceRepository.findByDataEmissao(dataEmissao);
+    }
+
+
+    public ValorTotalInvoice criarValorTotalInvoice(ValorTotalInvoice valorTotalInvoice){
+        return invoiceRepository.save(valorTotalInvoice);
+    }
+
+    public ValorTotalInvoice editarValorTotalInvoice(ValorTotalInvoice valorTotalInvoice){
+        return invoiceRepository.save(valorTotalInvoice);
+    }
+
+    public ValorTotalInvoice getValorTotalInvoice(ValorTotalInvoice valorTotalInvoice){
+        return invoiceRepository.findByValorTotalInvoice(valorTotalInvoice);
+    }
+
+    public void excluirValorTotalInvoice (ValorTotalInvoice valorTotalInvoice){
+        return invoiceRepository.deleteByValorTotalInvoice(valorTotalInvoice);
+    }
 }
