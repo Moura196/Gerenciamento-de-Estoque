bf49c0b9104b3f1ede6194c2cc71fea613affad7
June Skálletty <101154164+juneskalletty@users.noreply.github.com>
2025-05-10 14:28:28 -0300

    new branch
    new branch

--- Gerenciamento_Estoque_Desafio_3/src/main/java/estoque/desafio/gerenciamento/repositories/CompraRepository.java
+++
@@ -1,9 +0,0 @@
-package estoque.desafio.gerenciamento.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import estoque.desafio.gerenciamento.entities.Compra;
-
-public interface CompraRepository extends JpaRepository<Compra, Long> {
-
-}
