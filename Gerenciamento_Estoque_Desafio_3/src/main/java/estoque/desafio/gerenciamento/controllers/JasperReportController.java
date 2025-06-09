03ec2ec250c7dc9682cb772f519e0bc227f356d9
Moura196 <gmendesmoura@gmail.com>
2025-06-09 17:02:32 -0300

    feat: Altera código para geração de relatórios.

--- Gerenciamento_Estoque_Desafio_3/src/main/java/estoque/desafio/gerenciamento/controllers/JasperReportController.java
+++ Gerenciamento_Estoque_Desafio_3/src/main/java/estoque/desafio/gerenciamento/controllers/JasperReportController.java
@@ -2,14 +2,18 @@ package estoque.desafio.gerenciamento.controllers;
 
 import java.io.FileNotFoundException;
 
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import estoque.desafio.gerenciamento.entities.Fornecedor;
 import estoque.desafio.gerenciamento.entities.dtos.RelatorioDadosDTO;
 import estoque.desafio.gerenciamento.services.JasperReportService;
+import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
 @RequestMapping("/relatorio")
@@ -21,9 +25,16 @@ public class JasperReportController {
 		this.jasperReportService = jasperReportService;
 	}
 	
+	@Operation(summary = "Gera um relatório de uma compra:")
 	@PostMapping("/gerar")
-	public void gerarRelatorio(@RequestBody RelatorioDadosDTO dadosRelatorioRequest) throws FileNotFoundException {
-		this.jasperReportService.gerarRelatorio(dadosRelatorioRequest);
+	public ResponseEntity<?> gerarRelatorio(@RequestBody RelatorioDadosDTO dadosRelatorioRequest) throws FileNotFoundException {
+		try {
+			this.jasperReportService.gerarRelatorio(dadosRelatorioRequest);
+			return ResponseEntity.ok("Relatório gerado com sucesso!");
+		} catch (Exception e) {
+			return new ResponseEntity<>(e.getMessage(), HttpStatusCode.valueOf(504));
+			//"Erro ao gerar relatório", HttpStatusCode.valueOf(504)
+		}
 	}
 	
 }
